d6f9cda5afc534310f624df4f7098031f7835e71
Oleksii Nikolaiev <oleksii.nikolaiev.on@gmail.com>
2024-01-25 01:36:41 +0200

    Bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import { useSelector } from "react-redux";
+import { useGetUsersQuery } from "../redux/operations"; //!
+
 import { selectUsers, selectError, selectIsLoading } from "../redux/selectors";
 
 import UserForm from "./UserForm/UserForm";
 import UsersList from "./UsersList/UsersList";
 import { Notification } from "./App.styled";
+import { useEffect } from "react";
 
 const App = () => {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+
+  const { refetch } = useGetUsersQuery();
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
 
   const users = useSelector(selectUsers);
 
